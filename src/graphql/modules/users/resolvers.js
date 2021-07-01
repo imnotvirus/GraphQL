61559155575dfa50ee9b770cacfe555b277b38d1
Luiz Claudio <lcluizclaudio96@gmail.com>
2021-07-01 19:41:35 -0300

    websockets do user

--- src/graphql/modules/users/resolvers.js
+++ src/graphql/modules/users/resolvers.js
@@ -1,4 +1,5 @@
 import User from "../../../models/user";
+import { USER_ADDED } from "./channels";
 export default {
 	User: {
 		fullName: (user) => `${user.firstName} ${user.lastName}`,
@@ -8,9 +9,21 @@ export default {
 		user: (_, { id }) => User.findById(id),
 	},
 	Mutation: {
-		createUser: (_, { data }) => User.create(data),
+		createUser: async (_, { data }, { pubsub }) => {
+			const user = await User.create(data);
+
+			pubsub.publish(USER_ADDED, {
+				userAdded: user,
+			});
+			return user;
+		},
 		updateUser: (_, { id, data }) =>
 			User.findOneAndUpdate(id, data, { new: true }),
 		deleteUser: async (_, { id }) => !!(await User.findOneAndDelete(id)),
+	},
+	Subscription: {
+		userAdded: {
+			subscribe: (obj, args, { pubsub }) => pubsub.asyncIterator(USER_ADDED),
+		},
 	},
 };
--- src/startServer.js
+++ src/startServer.js
@@ -1,4 +1,4 @@
-import { ApolloServer } from "apollo-server";
+import { ApolloServer, PubSub } from "apollo-server";
 import mongoose from "mongoose";
 export default function startServer({ typeDefs, resolvers }) {
 	const uri = `mongodb+srv://${process.env.USER_DATABASE}:${process.env.PASSWORD_DATABASE}@graphql.3hsxm.mongodb.net/${process.env.NAME_DATABASE}?retryWrites=true&w=majority`;
@@ -9,8 +9,8 @@ export default function startServer({ typeDefs, resolvers }) {
 	} catch (error) {
 		console.log("erro no mongoose ");
 	}
-
-	const server = new ApolloServer({ typeDefs, resolvers });
+	const pubsub = new PubSub();
+	const server = new ApolloServer({ typeDefs, resolvers, context: { pubsub } });
 
 	server.listen().then(({ url }) => {
 		console.log(`🔥 Servidor iniciado no endereço: ${url}`);
